a09b851ab4d402d351bb3b10c88a6c7762d56c2a
Bjorn Peterson <76683417+bkpeterson@users.noreply.github.com>
2025-04-21 16:26:44 -0500

    commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { InstanceBase, Regex, runEntrypoint, InstanceStatus } from '@companion-module/base'
-import { UpdateActions } from './actions.js'
+const './actions.js'
 
 class ModuleInstance extends InstanceBase {
 	constructor(internal) {
