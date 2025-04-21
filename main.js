bad317d1151a4d43d66a5477f2bce3c9d72a52cb
Bjorn Peterson <76683417+bkpeterson@users.noreply.github.com>
2025-04-21 16:43:51 -0500

    Update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { InstanceBase, Regex, runEntrypoint, InstanceStatus } from '@companion-module/base'
-import { UpdateActions } from './actions.js'
+import { exec } from 'child_process'
 
 class ModuleInstance extends InstanceBase {
 	constructor(internal) {
@@ -10,8 +10,51 @@ class ModuleInstance extends InstanceBase {
 		this.config = config
 
 		this.updateStatus(InstanceStatus.Ok)
-		UpdateActions()
+
+		this.setActionDefinitions({
+			firetv: {
+				name: 'Fire TV Remote',
+				options: [
+					{
+						id: 'action',
+						type: 'dropdown',
+						label: 'Command',
+						choices: [
+							{ id: '19', label: 'Up'},
+							{ id: '20', label: 'Down'},
+							{ id: '21', label: 'Lef/Prev'},
+							{ id: '22', label: 'Right/Next'},
+							{ id: '66', label: 'Select/Play/Pause'},
+							{ id: '4',  label: 'Backl'},
+							{ id: '3',  label: 'Home'},
+							{ id: '1',  label: 'Menu'}
+						]
+					},
+				],
+				callback: async (event) => {
+					exec("./platform-tools/ubuntu/adb devices", (error, stdout, stderr) => {
+						const matchVar = new RegExp(this.config.host + ":" + this.config.port + "\sdevice", "g")
+						const connected = stdout.match(matchVar)
+						if(connected == null) {
+							exec(adbPath + " connect " + this.config.host + ":" + this.config.port);
+						}
+				
+						exec(cmd);
+					});
+				
+					exec(adbPath + " shell input keyevent " + event.options.action, (error, stdout, stderr) => {
+						if (error) {
+							console.error(`exec error: ${error}`);
+							return;
+						}
+						console.log(`stdout: ${stdout}`);
+						console.error(`stderr: ${stderr}`);
+					})
+				}
+			}
+		})
 	}
+
 	// When module gets deleted
 	async destroy() {
 		this.log('debug', 'destroy')
